b1951ced3da79479e199ad734614b79bfa75d2e5
bang9 <gusrn1423@naver.com>
2023-07-15 15:16:30 +0900

    feat: update prettier tabWith

--- packages/eslint-config/ts.react/index.js
+++ packages/eslint-config/ts.react/index.js
@@ -1,35 +1,35 @@
 module.exports = {
-    env: { browser: true, es6: true, node: true },
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended",
-        "plugin:react-hooks/recommended",
-        "prettier"
-    ],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        ecmaFeatures: { jsx: true },
-        ecmaVersion: 2018,
-        sourceType: "module"
-    },
-    plugins: ["react", "@typescript-eslint"],
-    rules: {
-        "no-unused-vars": "off",
-        "no-unexpected-multiline": "off",
-        "@typescript-eslint/explicit-module-boundary-types": "off",
-        "@typescript-eslint/no-unused-vars": ["error"],
-        "@typescript-eslint/no-non-null-assertion": ["error"],
-        "@typescript-eslint/ban-ts-ignore": "off",
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/camelcase": "off",
-        "@typescript-eslint/no-empty-function": "off",
-        "@typescript-eslint/no-use-before-define": "off",
-        "@typescript-eslint/interface-name-prefix": "off"
-    },
-    settings: {
-        react: { version: "detect" }
-    }
+  env: { browser: true, es6: true, node: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "no-unused-vars": "off",
+    "no-unexpected-multiline": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-unused-vars": ["error"],
+    "@typescript-eslint/no-non-null-assertion": ["error"],
+    "@typescript-eslint/ban-ts-ignore": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/camelcase": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-use-before-define": "off",
+    "@typescript-eslint/interface-name-prefix": "off"
+  },
+  settings: {
+    react: { version: "detect" }
+  }
 };
--- packages/prettier-config/ts/index.js
+++ packages/prettier-config/ts/index.js
@@ -1,12 +1,12 @@
 module.exports = {
-    printWidth: 120,
-    tabWidth: 4,
-    singleQuote: false,
-    bracketSpacing: true,
-    semi: true,
-    useTabs: false,
-    arrowParens: "avoid",
-    endOfLine: "auto",
-    jsxBracketSameLine: false,
-    trailingComma: "none"
+  printWidth: 120,
+  tabWidth: 2,
+  singleQuote: false,
+  bracketSpacing: true,
+  semi: true,
+  useTabs: false,
+  arrowParens: "avoid",
+  endOfLine: "auto",
+  jsxBracketSameLine: false,
+  trailingComma: "none"
 };
